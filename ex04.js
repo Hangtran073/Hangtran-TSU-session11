d956b1f2174be7efd93bf5f7f29cae06b802056d
Hangtran073 <hangt6465@gmail.com>
2025-03-03 20:05:40 +0900

    upload session11 bài 4

---
+++ ex04.js
@@ -0,0 +1,47 @@
+let number = parseInt(prompt("Nhập số bất kỳ từ 0 đến 9"));
+switch (number) {
+    case 0:
+        alert("Số không");
+        console.log("Số không");
+        break;
+    case 1:
+        alert("Số một");
+        console.log("Số một");
+        break;
+    case 2:
+        alert("Số hai");
+        console.log("Số hai");
+
+        break;
+    case 3:
+        alert("Số ba");
+        console.log("Số ba");
+        break;
+    case 4:
+        alert("Số bốn");
+        console.log("Số bốn");
+        break;
+    case 5:
+        alert("Số năm ");
+        console.log("Số năm");
+        break;
+    case 6:
+        alert("Số sáu");
+        console.log("Số sáu");
+        break;
+    case 7:
+        alert("Số bảy");
+        console.log("Số bảy");
+        break;
+    case 8:
+        alert("Số tám");
+        console.log("Số tám");
+        break;
+    case 9:
+        alert("Số chín");
+        console.log("Số chín");
+        break;
+    default:
+        console.log("Vui lòng nhập từ số 0 đến 9!");
+        break;
+}
